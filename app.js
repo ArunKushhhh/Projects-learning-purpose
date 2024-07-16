6669f7ec13504292660d776b786e19b4741f3188
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-07-16 13:36:47 +0530

    Added/page3

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+const fixedImage = document.querySelector("#fixed-image");
+
+const elemC = document.querySelector("#elem-container");
+elemC.addEventListener("mouseenter", function () {
+  fixedImage.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixedImage.style.display = "none";
+});
+
+const elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", () => {
+    var imageSrc = e.getAttribute("data-image");
+    fixedImage.style.backgroundImage = `url(${imageSrc})`;
+  });
 });
